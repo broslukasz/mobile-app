3b9fbd96f1fae89b17daf916ecf2f8ea62873042
Lukasz Bros <lbros@pgs-soft.com>
2022-04-27 15:14:17 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MobileApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
