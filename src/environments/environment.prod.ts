728ead60680352acfc4df85189347a432c4b8374
Lukasz Bros <lbros@pgs-soft.com>
2022-04-27 15:30:38 +0200

    after firebase install

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'mobile-pgs',
+    appId: '1:397594051848:web:fcf371bb8585ddc94158bd',
+    storageBucket: 'mobile-pgs.appspot.com',
+    apiKey: 'AIzaSyCJ-DdS9Nw_xow0VbOu93dnT5GP0Lek5Ng',
+    authDomain: 'mobile-pgs.firebaseapp.com',
+    messagingSenderId: '397594051848',
+  },
   production: true
 };
