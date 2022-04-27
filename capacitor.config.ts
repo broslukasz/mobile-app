afde1e8a90bc40754d767039f3aa3b6707578df1
Lukasz Bros <lbros@pgs-soft.com>
2022-04-27 15:42:01 +0200

    android init

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.pgs.mobile',
   appName: 'MobileApp',
   webDir: 'www',
   bundledWebRuntime: false
